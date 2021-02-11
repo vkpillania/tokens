add1e7c34924744cedd9a8b43a73c9b23450f45e
Vipin Kumar (Office) <vipin.kumar@tradeindia.com>
2021-02-11 22:49:30 +0530

    Initial commit token1

--- tokens/settings.py
+++ tokens/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
 '''
 import dj_database_url 
 token_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(token_db)
+DATABASES = { 'default' :token_db }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
